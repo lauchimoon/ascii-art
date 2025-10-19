c099ad2b9937ffcc3a12f1a73a6cd874a80197b9
lauchimoon <laurentino.luna06@gmail.com>
2025-10-19 17:25:04 -0300

    Add colored image support

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
     "fmt"
     "log"
+    "image"
+    "image/color"
     "image/png"
     "os"
 )
@@ -11,7 +13,7 @@ func main() {
     chars := " `'.,:;i+o*%&$#@"
     lenChars := len(chars)
 
-    f, err := os.Open("sample2.png")
+    f, err := os.Open("sample3.png")
     if err != nil {
         log.Fatalf("%v\n", err)
     }
@@ -22,10 +24,11 @@ func main() {
         log.Fatalf("%v\n", err)
     }
 
-    for y := 0; y < sample.Bounds().Dy(); y++ {
-        for x := 0; x < sample.Bounds().Dx(); x++ {
-            clr := sample.At(x, y)
-            r, g, b, _ := clr.RGBA()
+    imgGray := ToGrayscale(sample)
+
+    for y := 0; y < imgGray.Bounds().Dy(); y++ {
+        for x := 0; x < imgGray.Bounds().Dx(); x++ {
+            r, g, b, _ := imgGray.At(x, y).RGBA()
             var intensity int = int(r + g + b)
 
             intensity = intensity*lenChars / 768
@@ -33,6 +36,22 @@ func main() {
         }
         fmt.Println("")
     }
+}
+
+func ToGrayscale(img image.Image) image.Image {
+    newImg := image.NewRGBA(img.Bounds())
+    for y := 0; y < img.Bounds().Dy(); y++ {
+        for x := 0; x < img.Bounds().Dx(); x++ {
+            r, g, b, _ := img.At(x, y).RGBA()
+            luminosity := 0.299*float64(r) + 0.587*float64(g) + 0.114*float64(b) 
+
+            clr := color.RGBA{uint8(luminosity/256), uint8(luminosity/256), uint8(luminosity/256), 255}
+            newImg.Set(x, y, clr)
+        }
+    }
+
+
+    return newImg
 }
 
 func iabs(x int) int {
